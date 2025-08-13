bd2faff2fda6286654a1691eb22273a5bcf603b7
Akash s <akashsivakumarjayalekshmi@gmail.com>
2025-08-13 11:46:57 +0530

    Authentication

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router"
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">Navbar
+    
+    <Link to="/">
+    <p className="text-2xl font-bold text-gradient" >Resumind</p>
+    </Link>
+    <Link to="/upload" className="primary-button w-fit">
+    Upload resume</Link>
+    </nav>
+  )
+}
+
+export default Navbar
